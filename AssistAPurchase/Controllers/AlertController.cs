2c3cb42fd73a24b0f5191f781d5c80968a677058
Twinkal Parmar <66124568+TwinkalParmar-16@users.noreply.github.com>
2020-10-12 18:28:17 +0530

    Alert Added

---
+++ AssistAPurchase/Controllers/AlertController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using AssistAPurchase.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AssistAPurchase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlertController : ControllerBase
+    {
+        //Dictionary<int, AlertModel> querys = new Dictionary<int, AlertModel>();
+        private static List<AlertModel> querys;
+        //static int queryNumber;
+        // POST api/Mailing
+
+        public AlertController()
+        { 
+            querys =new List<AlertModel>();
+            //queryNumber = 0;
+        }
+        [HttpPost("ConfirmationAlert")]
+        public IActionResult SendAlert([FromBody] AlertModel body)
+        {
+            string message = "";
+            if (body == null)
+            {
+                message = "Unable to send alert!!.";
+                return NotFound(message);
+            }
+            message = body.CustomerName + " has booked following monitoring devices " + "\n" + body.ItemPurchased ;
+            return Ok(message);
+        }
+
+        [HttpPost("Query")]
+        public IActionResult QueryFromCustomer([FromBody] AlertModel body)
+        {
+            string message;
+            if (body==null)
+            {
+                message = "Unable to send Message.";
+                return NotFound(message);
+            }
+            message = "Message Sent!!";
+            querys.Add(body);
+            querys.Append(body);
+            //++queryNumber;
+            return Ok(querys);
+        }
+
+        [HttpPost("Query/{customerName}")]
+        public IActionResult AnswerFromPhilipsPersonnel(string customerName, [FromBody] AlertModel answer)
+        {
+
+            string message;
+            for (int i = 0; i < querys.Count; i++)
+            {
+                if (querys[i].CustomerName == customerName)
+                {
+                    message = "Question : " + querys[i].Question + "\n" + "Answer : " + answer.Answer;
+                    return Ok(message);
+                }
+            }
+            message = "Invalid Query Number";
+            return NotFound(querys);
+        }
+    }
+}
---
+++ AssistAPurchase/Models/AlertIModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AssistAPurchase.Models
+{
+    public class AlertModel
+    {
+        public string CustomerName { get; set; }
+        public string CustonmerMailId { get; set; }
+        public string ItemPurchased { get; set; }
+        public string CustomerphoneNumber { get; set; }
+        public string Question { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
