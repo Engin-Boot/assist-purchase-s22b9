021ad7bfbec699cd4d301e0074fe0c13e24cde40
GaganPunjabi1 <gagan.punjabi@philips.com>
2020-10-22 18:53:22 +0530

    Admin UI

--- AssistAPurchase_GUI/AssistAPurchase/src/app/app.module.ts
+++ AssistAPurchase_GUI/AssistAPurchase/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms' 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminComponent } from 'src/app/Component/admin/admin.component';
@@ -16,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [ProductManagementService],
   bootstrap: [AppComponent]
