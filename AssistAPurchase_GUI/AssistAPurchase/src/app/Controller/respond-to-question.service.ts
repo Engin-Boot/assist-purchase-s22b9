673b765a9cd6c456228090a5ebb7c0086a4003fe
kavyashukla18 <kavya.shukla@philips.com>
2020-10-26 11:26:59 +0530

    chat bot

--- AssistAPurchase_GUI/AssistAPurchase/src/app/Controller/respond-to-question.service.ts
+++ AssistAPurchase_GUI/AssistAPurchase/src/app/Controller/respond-to-question.service.ts
@@ -6,11 +6,16 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 })
 export class RespondToQuestionService {
 
-  public url: string = 'https://localhost:5001/api/RespondToQuestions/MonitoringProductHomePage/';
+  public url: string = 'http://localhost:5000/api/RespondToQuestions/MonitoringProductHomePage/';
   constructor( private https: HttpClient ) { }
 
   GetProductByProductNumber(prodNumber : string){
 
     return this.https.get(this.url + 'Description/{productNumber}');
+  }
+
+  GetSpecificProduct(body: any){
+
+    return this.https.post('http://localhost:5000/api/RespondToQuestions/MonitoringProduct', body);
   }
 }
