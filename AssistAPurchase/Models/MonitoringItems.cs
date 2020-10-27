45acfa7ef7e115295c2634a912a9e3de6750f2e5
GaganPunjabi1 <gagan.punjabi@philips.com>
2020-10-27 18:15:25 +0530

    reshaper

--- AssistAPurchase/Models/MonitoringItems.cs
+++ AssistAPurchase/Models/MonitoringItems.cs
@@ -18,7 +18,7 @@
             public string ScreenSize { get; set; }
             public string MultiPatientSupport { get; set; }
             public string CyberSecurity { get; set; }
-            // ReShaper disable once
-            public string Image { get; set; }
+        // ReShaper disable once
+        public string Image;
     }
 }
